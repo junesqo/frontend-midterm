a6a1f0b6e834822375c34c54cffdc0c9e4656146
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2023-11-01 01:02:39 +0600

    app js changed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import ColorChangingTextArea from './components/ColorChangingTextArea';
 function App() {
   return (
     <div className="App">
-      <h1>Мой React Приложение</h1>
-      <ColorChangingTextArea /> {/* Используйте ColorChangingTextArea компонент здесь */}
+      <h1>Моё React Приложение</h1>
+      <ColorChangingTextArea />
     </div>
   );
 }
