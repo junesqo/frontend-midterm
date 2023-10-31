b0a9c4c1f8588b6dace0f9c43461dc99cd9a0a81
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2023-11-01 01:31:05 +0600

    Алгоритм поменял

--- src/components/ColorChangingTextArea.jsx
+++ src/components/ColorChangingTextArea.jsx
@@ -8,9 +8,13 @@ const ColorChangingTextArea = () => {
     const newText = event.target.value;
     setText(newText);
 
-    const lastChar = newText.slice(-1);
-    const newColor = `#${lastChar.charCodeAt(0).toString(16).repeat(3)}`;
-    setBackgroundColor(newColor);
+    const colors = ['#ff9999', '#99ff99', '#9999ff', '#ffff99', '#ff99ff', '#99ffff']; // Список цветов
+    const colorIndex = newText.length % colors.length; // Определяем индекс цвета в зависимости от длины текста
+    setBackgroundColor(colors[colorIndex]);
+    
+    // const lastChar = newText.slice(-1);
+    // const newColor = `#${lastChar.charCodeAt(0).toString(16).repeat(3)}`;
+    // setBackgroundColor(newColor);
   };
 
   return (
