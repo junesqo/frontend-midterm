0ae7574c0f093bfe4b2f7439be2a698c9960d2b0
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2023-11-01 01:54:15 +0600

    Лого jsx

---
+++ src/components/AnimatedLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AnimatedLogo.css';
+
+const AnimatedLogo = () => {
+  return (
+    <div className="animated-logo-container">
+      <img src="logo.png" alt="Animated Logo" className="animated-logo" />
+    </div>
+  );
+};
+
+export default AnimatedLogo;
--- src/components/ColorChangingTextArea.jsx
+++ src/components/ColorChangingTextArea.jsx
@@ -8,10 +8,10 @@ const ColorChangingTextArea = () => {
     const newText = event.target.value;
     setText(newText);
 
-    const colors = ['#ff9999', '#99ff99', '#9999ff', '#ffff99', '#ff99ff', '#99ffff']; // Список цветов
-    const colorIndex = newText.length % colors.length; // Определяем индекс цвета в зависимости от длины текста
+    const colors = ['#ff9999', '#99ff99', '#9999ff', '#ffff99', '#ff99ff', '#99ffff'];
+    const colorIndex = newText.length % colors.length;
     setBackgroundColor(colors[colorIndex]);
-    
+
     // const lastChar = newText.slice(-1);
     // const newColor = `#${lastChar.charCodeAt(0).toString(16).repeat(3)}`;
     // setBackgroundColor(newColor);
