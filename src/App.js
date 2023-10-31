bf76cec1a640a18fd9a41c7cb9e7772324eb5c03
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2023-11-01 02:15:23 +0600

    Лого добавил

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ColorChangingTextArea from './components/ColorChangingTextArea';
+import AnimatedLogo from './components/AnimatedLogo';
 
 function App() {
   return (
     <div className="App">
       <h1>Моё React Приложение</h1>
+      <AnimatedLogo />
       <ColorChangingTextArea />
     </div>
   );
--- src/components/AnimatedLogo.jsx
+++ src/components/AnimatedLogo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './AnimatedLogo.css';
+import logo from './logo.png';
 
 const AnimatedLogo = () => {
   return (
     <div className="animated-logo-container">
-      <img src="logo.png" alt="Animated Logo" className="animated-logo" />
+      <img src={logo} alt="Animated Logo" className="animated-logo" />
     </div>
   );
 };
